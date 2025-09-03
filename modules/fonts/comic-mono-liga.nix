20f02f4e41cc19871858bd62c9b92caee4e8779c
BrokenC1oud <jgbsxx20130315@gmail.com>
2025-09-03 18:59:53 +0800

    fill hash

--- modules/fonts/comic-mono-liga.nix
+++ modules/fonts/comic-mono-liga.nix
@@ -9,7 +9,7 @@
     owner = "wayou";
     repo = "comic-mono-font";
     rev = "2961b5d7c8ec497f6e8e50c5b9cb136af49de325";
-    hash = "";
+    hash = "sha256-Zv3jMDidwbzFbY7rkJ5DnaHsNPYQsN2znSVTB0kQ6Y0=";
   };
 
   installPhase = ''
