ac6cd4866f9ce87c7d22e25212dcd953136b724c
BrokenC1oud <jgbsxx20130315@gmail.com>
2025-09-03 18:07:04 +0800

    correct grammar

--- home/default.nix
+++ home/default.nix
@@ -24,6 +24,13 @@
     settings = {
       preset = "transgender";
       mode = "rgb";
+      color_align = {
+        mode = "custom";
+	custom_colors = {
+	  "1" = 1;
+	  "2" = 0;
+	};
+      };
       backend = "fastfetch";
     };
   };
