0168c747ef3116240b6dd8585f5be34a276f758b
BrokenC1oud <jgbsxx20130315@gmail.com>
2026-02-24 21:01:09 +0800

    sway

--- home/desktop/sway.nix
+++ home/desktop/sway.nix
@@ -1,6 +1,25 @@
-{ ... }:
+{ pkgs, ... }:
 {
   wayland.windowManager.sway = {
     enable = true;
+
+    config = rec {
+      modifier = "Mod4";
+      terminal = "kitty";
+    };
   };
+
+  programs.waybar = {
+    enable = true;
+    settings = [
+      {
+        layer = "top";
+        position = "top";
+      }
+    ];
+  };
+
+  home.packages = with pkgs; [
+    wl-clipboard
+  ];
 }
