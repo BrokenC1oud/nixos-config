6a370d3b6997b64ec22401faa517aaf5ad4ebc39
BrokenC1oud <jgbsxx20130315@gmail.com>
2026-02-24 16:01:35 +0800

    sway

--- modules/packages.nix
+++ modules/packages.nix
@@ -24,8 +24,8 @@
     extraPackages = with pkgs; [
       wl-clipboard
     ];
-    environment.sessionVariables.NIXOS_OZONE_WL = "1";
   };
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
   nixpkgs.config.allowUnfree = true;
 }
