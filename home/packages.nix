9619b99f3443884ca2cdb519d7f0658a064d60be
BrokenC1oud <jgbsxx20130315@gmail.com>
2026-02-24 08:19:49 +0800

    typo

--- home/packages.nix
+++ home/packages.nix
@@ -3,7 +3,7 @@
   home.packages = with pkgs; [
     clash-verge-rev
     gdu
-    (gimp-gimp-with-plugins.override {
+    (gimp-with-plugins.override {
       plugins = [ gimpPlugins.resynthesizer ];
     })
     libreoffice-fresh
