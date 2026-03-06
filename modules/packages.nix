455cbda54eeb1207426d1cdefc10d51e8cd62e01
BrokenC1oud <jgbsxx20130315@gmail.com>
2026-03-06 12:44:01 +0800

    podman

--- modules/packages.nix
+++ modules/packages.nix
@@ -33,7 +33,7 @@
   virtualisation.docker.enable = false;
   virtualisation.podman.enable = true;
   virtualisation.podman.dockerSocket.enable = true;
-  virtualisation.podman.defaultNetwork.dnsname.enable = true;
+  virtualisation.podman.defaultNetwork.settings.dns_enabled = true;
 
   nixpkgs.config.allowUnfree = true;
 }
